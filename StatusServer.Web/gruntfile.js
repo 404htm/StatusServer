105ac32147e068f091c217c11d948f2dcf296e7b
github@404htm.com <github@404htm.com>
2015-07-30 21:09:06 -0400

    Setup less compilier

--- StatusServer.Web/gruntfile.js
+++ StatusServer.Web/gruntfile.js
@@ -9,8 +9,36 @@
 				}
 			}
 		},
+		less: {
+			development: {
+				options: {
+					compress: false,
+				},
+				files: { "wwwroot/css/styles.css": "wwwroot/css/styles.less" }
+			},
+			production: {
+				options: {
+					compress: true,
+				},
+				files: { "wwwroot/css/styles.css": "wwwroot/css/styles.less" }
+			}
+		},
+		watch: {
+			styles: {
+				files: ['css/**/*.less'], // which files to watch
+				tasks: ['less'],
+				options: {
+					nospawn: true
+				}
+			}
+		}
 	});
 
-	grunt.registerTask("default", ["bower:install"]);
+	
+
 	grunt.loadNpmTasks("grunt-bower-task");
+	grunt.loadNpmTasks('grunt-contrib-less');
+
+	grunt.registerTask("default", ["bower:install"]);
+	grunt.registerTask('default', ['less']);
 };
