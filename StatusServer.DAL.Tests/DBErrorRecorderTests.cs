edfbc80472013ebd7a3e0eb0129d2838a5389df1
github@404htm.com <github@404htm.com>
2015-08-19 22:42:00 -0400

    Stubbing out DAL Tests

---
+++ StatusServer.DAL.Tests/DBErrorRecorderTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StatusServer.DAL.Tests
+{
+	[TestClass]
+	public class DBErrorRecorderTests
+	{
+		[TestMethod]
+		public void Log_exception_message()
+		{
+			var logger = new DBErrorRecorder();
+			var token = Guid.NewGuid();
+
+			logger.Log(token, new InvalidCastException(), "Test Exception");
+
+
+		}
+	}
+}
