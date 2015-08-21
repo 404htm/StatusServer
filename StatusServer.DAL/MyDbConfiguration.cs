d9ff3e5c647d198ac3bc6f449e85aeda1c34c0fb
github@404htm.com <github@404htm.com>
2015-08-20 23:12:53 -0400

    Ugh - Trying to make EF and vnext play nice... trying

---
+++ StatusServer.DAL/MyDbConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.SqlServer;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatusServer.DAL
+{
+	public class MyDbConfiguration : DbConfiguration
+	{
+		public MyDbConfiguration()
+		{
+			//var ds = (DataSet)ConfigurationManager.GetSection("system.data");
+			SetExecutionStrategy("System.Data.SqlClient", () => new SqlAzureExecutionStrategy());
+			SetDefaultConnectionFactory(new LocalDbConnectionFactory("v11.0"));
+			
+			
+		}
+	}
+}
