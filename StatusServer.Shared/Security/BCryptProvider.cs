20d7d8b96a67a6441bf00de3e520f29acfc02634
Kelly Robins <kelly.robins@troyweb.com>
2015-10-02 00:39:06 -0400

    Added BCrypt Security provider / Security base types

---
+++ StatusServer.Shared/Security/BCryptProvider.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using static BCrypt.Net.BCrypt;
+
+namespace StatusServer.Shared.Security
+{
+	public class BCryptProvider : SecurityProvider
+	{
+		const int WORK_FACTOR = 8;
+		static BCryptProvider()
+		{
+			ProviderType = "BCrypt";
+        }
+
+		public override string GenerateApiPassword()
+		{
+			return BCrypt.Net.BCrypt.GenerateSalt(WORK_FACTOR).Substring(0, PassLength);
+		}
+
+		public override byte[] GenerateSalt()
+		{
+			var strSalt = BCrypt.Net.BCrypt.GenerateSalt(WORK_FACTOR);
+			return Convert.FromBase64String(strSalt);
+        }
+
+		public override byte[] HashPassword(string password, byte[] salt)
+		{
+			var strSalt = Convert.ToBase64String(salt);
+			var strHash = BCrypt.Net.BCrypt.HashPassword(password + strSalt, WORK_FACTOR);
+			return Convert.FromBase64String(strHash);
+        }
+
+		public override bool VerifyPassword(string password, byte[] salt, byte[] hash)
+		{
+			var compare_hash = HashPassword(password, salt);
+			return hash.SequenceEqual(compare_hash);
+		}
+	}
+}
---
+++ StatusServer.Shared/Security/SecurityProvider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatusServer.Shared.Security
+{
+	public abstract class SecurityProvider
+	{
+		protected int SaltLength = 60;
+		protected int PassLength = 10;
+
+		public static string ProviderType { get; protected set; }
+
+		public abstract string GenerateApiPassword();
+
+		public abstract byte[] GenerateSalt();
+
+		public abstract byte[] HashPassword(string password, byte[] salt);
+
+		public abstract bool VerifyPassword(string password, byte[] salt, byte[] hash);
+
+	}
+}
